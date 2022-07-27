cc583e14d15114e15cd4fdb13d17cb9d1f35a41b
mbhossain <king.cseiu@gmail.com>
2022-07-27 15:44:37 +0600

    ngClass

--- src/app/components/directives/directives.component.ts
+++ src/app/components/directives/directives.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./directives.component.css']
 })
 export class DirectivesComponent implements OnInit {
-
-  public ngSwitchtitle = 'ngSwitch';
+  /*Note:
+    There are mainly 3 important element of ngSwitch-
+    1. ngSwitch.
+    2. ngSwitchCase.
+    3. ngSwitchDefault.*/
+  public ngSwitchTitle = 'ngSwitch';
   public color = 'red';
   public weekDay = 'Saturday';
+
+  /*Note:
+  We can pass dynamic value via variales like-
+  1. ngClass with string.
+  2. ngClass with array.
+  3. ngClass with object.
+  4 .ngClass with component method.*/
+  public ngClassTitle = 'ngClass';
+  public colorClassName = 'colorClass';
+  public backgroundClassName = 'backgroundClass';
 
   constructor() { }
 
