ec96abde05ef5f259d6abd33a832a19cd136043f
mbhossain <king.cseiu@gmail.com>
2022-09-01 16:11:51 +0600

    login API implementation

--- src/app/modules/authentication/components/login/login.component.ts
+++ src/app/modules/authentication/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public loginUserData = {
+    email: null,
+    password: null
+  }
 
-  constructor() { }
+  constructor(private _auth: AuthService) { }
 
   ngOnInit(): void {
+  }
+
+  loginUser() {
+    this._auth.loginUser(this.loginUserData)
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err)
+      )
   }
 
 }
--- src/app/modules/authentication/services/auth.service.ts
+++ src/app/modules/authentication/services/auth.service.ts
@@ -5,12 +5,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private _registerUrl = "http://localhost:3000/api/register"
+  private _registerUrl = "http://localhost:3000/api/register";
+  private _loginUrl = "http://localhost:3000/api/login";
 
   constructor(private http: HttpClient) { }
 
   registerUser(user: any) {
     return this.http.post<any>(this._registerUrl, user)
+  }
+
+  loginUser(user: any) {
+    return this.http.post<any>(this._loginUrl, user)
   }
 
 }
