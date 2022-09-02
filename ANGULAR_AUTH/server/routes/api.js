1c4c44c9eff292a5a5be0d02113a4b71d8704c6d
mbhossain <king.cseiu@gmail.com>
2022-09-02 16:57:01 +0600

    Middleware to Verify Token

--- ANGULAR_AUTH/server/routes/api.js
+++ ANGULAR_AUTH/server/routes/api.js
@@ -15,6 +15,25 @@ mongoose.connect(db, err => {
     }
 })
 
+function verifyToken(req, res, next) {
+    if (!req.headers.authorization) {
+        return res.status(401).send('Unauthorized request')
+    }
+
+    let token = req.headers.authorization.split(' ')[1]
+    if (token === 'null') {
+        return res.status(401).send('Unauthorized request')
+    }
+
+    let payload = jwt.verify(token, 'secretKey')
+    if (!payload) {
+        return res.status(401).send('Unauthorized request')
+    }
+
+    req.userId = payload.subject
+    next()
+}
+
 router.get('/', (req, res) => {
     res.send('From API route')
 })
@@ -95,7 +114,7 @@ router.get('/events', (req, res) => {
     res.json(events)
 })
 
-router.get('/special', (req, res) => {
+router.get('/special', verifyToken, (req, res) => {
     let specialEvents = [
         {
             "_id": "1",
--- src/app/modules/authentication/components/special-events/special-events.component.ts
+++ src/app/modules/authentication/components/special-events/special-events.component.ts
@@ -1,5 +1,7 @@
 /* Angular Stuff */
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 /* 3rd party libraries */
 
@@ -14,13 +16,21 @@ import { EventService } from '../../services/event.service';
 export class SpecialEventsComponent implements OnInit {
   public specialEvents: any = []
 
-  constructor(private _eventService: EventService) { }
+  constructor(
+    private _eventService: EventService
+    , private _router: Router) { }
 
   ngOnInit(): void {
     this._eventService.getSpecialEvents()
       .subscribe(
         res => this.specialEvents = res,
-        err => console.log(err)
+        err => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 401) {
+              this._router.navigate(['/authentication/login'])
+            }
+          }
+        }
       )
   }
 
