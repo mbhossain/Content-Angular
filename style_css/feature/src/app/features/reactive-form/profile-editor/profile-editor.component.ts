819c409d96147b1d27ac7585fb6bf7fef71405f4
mbhossain <king.cseiu@gmail.com>
2022-12-12 14:43:17 +0600

    nested form group

--- style_css/feature/src/app/features/reactive-form/profile-editor/profile-editor.component.ts
+++ style_css/feature/src/app/features/reactive-form/profile-editor/profile-editor.component.ts
@@ -10,11 +10,26 @@ export class ProfileEditorComponent {
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
   });
 
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
   }
 
 }
